29eadb2719ae5b9a48567a4d3e5692532799b895
willise <sunshuai@cmss.chinamobile.com>
2018-12-13 09:38:15 +0800

    Fix

--- sslocal/docker-entrypoint.sh
+++ sslocal/docker-entrypoint.sh
@@ -5,7 +5,7 @@ cat > /etc/shadowsocks/config << EOF
 {
     "server": "$SERVER",
     "server_port": "$SERVER_PORT",
-    "method": "aes-256-cfb",
+    "method": "aes-128-cfb",
     "password": "$PASSWORD",
     "fast_open": false,
     "local_address": "0.0.0.0",
